724c0ecd263f037eafae66690ff8867ecfd1f534
Quinn Damerell <quinnd@outlook.com>
2025-01-08 20:47:43 -0800

    Adding the new Sage protocol for Home Assistant Assist!

--- Gen.cmd
+++ Gen.cmd
@@ -2,6 +2,7 @@ del /S /Q cs\*
 mkdir cs
 pushd cs
 ..\flatc_build\flatc.exe --csharp ..\HomewayProtocol.fbs
+..\flatc_build\flatc.exe --csharp ..\SageProtocol.fbs
 del /S /Q ..\..\Homeway.Service\Homeway\Clients\Proto\*
 xcopy .\homeway\Proto\ ..\..\Homeway.Service\Homeway\Clients\Proto\ /E/H
 popd
@@ -10,6 +11,7 @@ del /S /Q py\*
 mkdir py
 pushd py
 ..\flatc_build\flatc.exe --python-typing --python ..\HomewayProtocol.fbs
+..\flatc_build\flatc.exe --python-typing --python ..\SageProtocol.fbs
 del /S /Q ..\..\Homeway.Addon\homeway\homeway\Proto
 xcopy .\homeway\Proto\ ..\..\Homeway.Addon\homeway\homeway\Proto\ /E/H
 popd
